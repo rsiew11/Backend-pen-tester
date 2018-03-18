ac95237e8453971b10b5ea73dffb72a9d328d597
rsiew11 <rsiew11@gmail.com>
2018-03-18 13:10:01 -0400

    now creates x amount of tabs

--- formfuzzer/popup.js
+++ formfuzzer/popup.js
@@ -11,7 +11,6 @@ function upArrow(){
     n++;
     localStorage.setItem('counter', n);
     document.getElementById('counter').innerHTML = n;
-    console.log("jldkfkd");
 
 }
 
@@ -92,12 +91,27 @@ function startCSRF(){
 
 //------------------------------------------------------------------------------
 function startFuzzing(fuzLen, iterCount){
-
-    for (var i = 0; i < iterCount; i++) {
+    console.log(fuzLen);
+    console.log(iterCount);
+    console.log("LDSLKJFJDSKFDKSJFKLDSJKFJDSKFJDKFJKLSDJFKLDJFKDLS");
+    for (let i = 0; i < iterCount; i++) {
         //TODO:
         // open iterCount number of tabs... each with different payload
-        chrome.tabs.create({'url': "http://192.168.1.29/"}); // right now website is hardcoded...
-        //now need to wait for the page to be loaded
+        //chrome.tabs.create({'url': "http://192.168.1.29/"}); // right now website is hardcoded...
+        console.log(i);
+        chrome.tabs.create({'url': "https://wordpress.com"}); // test website for now
+    }
+    //now need to wait for the pages to be loaded
+
+    chrome.tabs.query( {},function(tabs){
+
+        for (let i = 0; i<iterCount; i++){
+            //TODO: loop throught the tabs and inject a payload into each
+            //chrome.tabs.executeScript(tabs[i].id,{file: ""})
+
+        }
+
+    });
 
 /*
         chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
@@ -118,12 +132,11 @@ function startFuzzing(fuzLen, iterCount){
         });
 */
 
-        fillForm(createString(fuzLen));
+        //fillForm(createString(fuzLen));
 
 
         // payload inserted in all of the forms
         // and then all submitted
-    }
 
 }
 
@@ -132,17 +145,18 @@ function startFuzzing(fuzLen, iterCount){
 
 // runs the test based on radio buttons
 function startTest(){
+    console.log("lsdjfkldsjdjf");
 
-    var len = document.getElementById('setLen').value;
+    var len = Number(document.getElementById('injectLen').value);
     //checking value of setLen
-    if((typeof(len) != "number") || (len <= 0)){
+    if((len >= 1000) || (len <= 0)){
         //TODO: the alert closes instantly
         alert("enter a valid value for the string Length")
     }
 
-    var iterations = document.getElementById('setCount').value;
+    var iterations = Number(document.getElementById('iterCount').value);
     //checking value of iterCount
-    if((typeof(iterations) != "number") || (iterations <= 0)){
+    if((iterations >= 1000)|| (iterations <= 0)){
         //TODO: the alert closes instantly
         alert("enter a valid value for the number of iterations")
     }
