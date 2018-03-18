57d506570dcb86cfc2988fb97c5aecfea7271bb7
rsiew11 <rsiew11@gmail.com>
2018-03-17 22:42:06 -0400

    adding some framework to popup

--- formfuzzer/popup.js
+++ formfuzzer/popup.js
@@ -11,6 +11,7 @@ function upArrow(){
     n++;
     localStorage.setItem('counter', n);
     document.getElementById('counter').innerHTML = n;
+    console.log("jldkfkd");
 
 }
 
@@ -30,7 +31,31 @@ function changeBackgroundColor() {
     chrome.tabs.executeScript(null,
     {code: script});
 }
+///////////////////////----------------------------------///////////////////////
+///////////////////////----------------------------------///////////////////////
+///////////////////////----------------------------------///////////////////////
 
+
+//generates a single random charcter
+function randomChar(){
+    var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz=+-_)(*&^%$#@!~<,>.?:;[]{}";
+    var r = Math.floor(Math.random() * chars.length);
+    return chars.substring(r,r+1);
+}
+
+//------------------------------------------------------------------------------
+//--------------------The Various Tests Functions-------------------------------
+//------------------------------------------------------------------------------
+function startSQL(){
+
+}
+
+//------------------------------------------------------------------------------
+function startCSRF(){
+
+}
+
+//------------------------------------------------------------------------------
 function startFuzzing(){
     var iterCount = document.getElementById('startFuzzing').value;
 
@@ -46,15 +71,30 @@ function startFuzzing(){
         // and then all submitted
     }
 
+}
+
+//---------------------------End of Test Functions------------------------------
+//------------------------------------------------------------------------------
 
 
+function startTest(){
+    //check which radio button is selected
+    if(document.getElementById('selectSQL').checked){
+        startSQL();
+    }
+    else if(document.getElementById('selectCSRF').checked){
+        startCSRF();
+    }
+    else if(document.getElementById('selectFuzzing').checked){
+        startFuzzing();
+    }
 
 }
 
 
 
 document.getElementById('setColor').addEventListener('click',changeBackgroundColor);
-document.getElementById('startFuzzing').addEventListener('click',startFuzzing);
+document.getElementById('startFuzzing').addEventListener('click',startTest);
 
 document.getElementById('new_tab').addEventListener('click',newTab);
 document.getElementById('new_window').addEventListener('click',newWindow);
