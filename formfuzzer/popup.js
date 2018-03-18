7a00161d6422dd4ba58fad17766fd78ba630d696
rsiew11 <rsiew11@gmail.com>
2018-03-18 02:01:30 -0400

    added some more stuff and setup the structure of the extension

--- formfuzzer/popup.js
+++ formfuzzer/popup.js
@@ -35,6 +35,11 @@ function changeBackgroundColor() {
 ///////////////////////----------------------------------///////////////////////
 ///////////////////////----------------------------------///////////////////////
 
+//------------------------------------------------------------------------------
+//-----------------------Helper Functions For The Tests-------------------------
+//------------------------------------------------------------------------------
+
+
 
 //generates a single random charcter
 function randomChar(){
@@ -44,29 +49,78 @@ function randomChar(){
 }
 
 //------------------------------------------------------------------------------
+function createString(len){
+    var s = '';
+    for (var i=0; i<len; i++){
+        s.concat(randomChar());
+    }
+    return s;
+}
+
+//------------------------------------------------------------------------------
+function fillForm(s){
+    //TODO
+    //find the submit/search bar and fill it out
+    chrome.tabs.executeScript(null,
+    {
+        code:"document.forms[0]['q'].value=".concat(s)
+    });
+
+}
+//------------------------------------------------------------------------------
+
+
+//------------------------------------------------------------------------------
 //--------------------The Various Tests Functions-------------------------------
 //------------------------------------------------------------------------------
-function startSQL(){
+function startSQL(iterCount){
+    //TODO: the whole thing
+
+    for (var i=0; i<iterCount; i++){
+        //shit
+
+
+    }
 
 }
 
 //------------------------------------------------------------------------------
 function startCSRF(){
+    //TODO: the whole thing
 
 }
 
 //------------------------------------------------------------------------------
-function startFuzzing(){
-    var iterCount = document.getElementById('startFuzzing').value;
-
-    //checking value of itercount
-    if((typeof(iterCount) != "number") || (iterCount <= 0)){
-        alert("enter a valid value for the number of iterations")
-        return;
-    }
+function startFuzzing(fuzLen, iterCount){
 
     for (var i = 0; i < iterCount; i++) {
+        //TODO:
         // open iterCount number of tabs... each with different payload
+        chrome.tabs.create({'url': "http://192.168.1.29/"}); // right now website is hardcoded...
+        //now need to wait for the page to be loaded
+
+/*
+        chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+            // make sure the status is 'complete' and it's the right tab
+            if (tab.url.indexOf('127.0.0.1:8000') != -1 && changeInfo.status == 'complete') {
+                chrome.tabs.executeScript(null, {
+                    code: "alert('hi');"
+                });
+            }
+        }
+
+        OR!!!!!!!
+
+        chrome.tabs.onUpdated.addListener(function (tabId , info) {
+            if (info.status === 'complete') {
+                // your code ...
+            }
+        });
+*/
+
+        fillForm(createString(fuzLen));
+
+
         // payload inserted in all of the forms
         // and then all submitted
     }
@@ -76,17 +130,32 @@ function startFuzzing(){
 //---------------------------End of Test Functions------------------------------
 //------------------------------------------------------------------------------
 
-
+// runs the test based on radio buttons
 function startTest(){
+
+    var len = document.getElementById('setLen').value;
+    //checking value of setLen
+    if((typeof(len) != "number") || (len <= 0)){
+        //TODO: the alert closes instantly
+        alert("enter a valid value for the string Length")
+    }
+
+    var iterations = document.getElementById('setCount').value;
+    //checking value of iterCount
+    if((typeof(iterations) != "number") || (iterations <= 0)){
+        //TODO: the alert closes instantly
+        alert("enter a valid value for the number of iterations")
+    }
+
     //check which radio button is selected
     if(document.getElementById('selectSQL').checked){
-        startSQL();
+        startSQL(iterations);
     }
     else if(document.getElementById('selectCSRF').checked){
         startCSRF();
     }
     else if(document.getElementById('selectFuzzing').checked){
-        startFuzzing();
+        startFuzzing(len,iterations);
     }
 
 }
@@ -94,7 +163,7 @@ function startTest(){
 
 
 document.getElementById('setColor').addEventListener('click',changeBackgroundColor);
-document.getElementById('startFuzzing').addEventListener('click',startTest);
+document.getElementById('startTest').addEventListener('click',startTest);
 
 document.getElementById('new_tab').addEventListener('click',newTab);
 document.getElementById('new_window').addEventListener('click',newWindow);
